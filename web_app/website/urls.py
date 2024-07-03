61ac1e61255e1059a2b77cc235343deaf57959ec
Francisco Antunes <153289503+Antunes233@users.noreply.github.com>
2024-07-03 23:56:46 +0100

    Update urls.py

--- web_app/website/urls.py
+++ web_app/website/urls.py
@@ -14,5 +14,7 @@ urlpatterns = [
     path("details/patient/<int:id>/session_<int:session_id>", views.SessionReview, name="sessionreview"),
     path('get-data-points/<int:start>/<int:count>/<int:id>/<int:session_id>/', views.get_data_points, name='get_data_points'),
     path('get-expected-curve/<int:id>/', views.get_expected_curve, name='get_expected_curve'),
+    path('get-data-between-indexes-right/<int:id>/<int:session_id>/<int:start_index>/<int:end_index>/', views.get_data_between_indexes_right, name='get_data_between_indexes_right'),
+    path('get-data-between-indexes-left/<int:id>/<int:session_id>/<int:start_index>/<int:end_index>/', views.get_data_between_indexes_left, name='get_data_between_indexes_left'),
+    path('get-all-data-points/<int:id>/<int:session_id>/', views.get_all_data_points, name='get_all_data_points'),
 ]
-
